3d79271a449d1c76319627db0b070cc6fd7a072b
Jerome Magpantay <jerome.magpantay247@gmail.com>
2024-02-19 23:54:50 -0500

    added crud functionality

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # A post has a title, author, and body.
 
@@ -10,4 +11,5 @@ class Post(models.Model):
     def __str__(self) -> str:
         return self.title
     
-    
+    def get_absolute_url(self):
+        return reverse('post_details', args=[str(self.id)])
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import DetailView, ListView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, DeleteView, ListView, UpdateView
 
 
 # Shows a specific blog post.
@@ -13,5 +14,17 @@ class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
 
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
 
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
 
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
